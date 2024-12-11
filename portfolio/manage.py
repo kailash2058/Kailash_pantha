acc419a490008ce54c48d2a94c7b6ad7dbd8d243
kailash pantha <076bei017.kailash@pcampus.edu.np>
2024-12-11 20:50:22 +0545

    refactored

--- portfolio/manage.py
+++ portfolio/manage.py
@@ -4,29 +4,43 @@ import os
 import sys
 
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+# def main():
+#     # """Run administrative tasks."""
+#     # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
+#     # try:
+#     #     from django.core.management import execute_from_command_line
+#     # except ImportError as exc:
+#     #     raise ImportError(
+#     #         "Couldn't import Django. Are you sure it's installed and "
+#     #         "available on your PYTHONPATH environment variable? Did you "
+#     #         "forget to activate a virtual environment?"
+#     #     ) from exc
+#     # execute_from_command_line(sys.argv)
 
 
+#     if "gunicorn" in sys.argv:
+#         from gunicorn.app.wsgiapp import run
+#         sys.argv = ["gunicorn", "portfolio.wsgi:application"]
+#         run()
+#     else:
+#         from django.core.management import execute_from_command_line
+#         execute_from_command_line(sys.argv)
+
+
+
+# if __name__ == '__main__':
+#     main()
+
+
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings")
+    
+    # Check if the command includes "gunicorn"
     if "gunicorn" in sys.argv:
-        from gunicorn.app.wsgiapp import run
-        sys.argv = ["gunicorn", "portfolio.wsgi:application"]
-        run()
+        from gunicorn.app.wsgiapp import run  # Import Gunicorn's WSGI app
+        sys.argv = ["gunicorn", "portfolio.wsgi:application"]  # Set arguments for Gunicorn
+        run()  # Run Gunicorn
     else:
         from django.core.management import execute_from_command_line
-        execute_from_command_line(sys.argv)
-
-
-
-if __name__ == '__main__':
-    main()
+        execute_from_command_line(sys.argv)  # Run Django's default commands
