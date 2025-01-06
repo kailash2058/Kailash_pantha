035dbaa90576798722f92c6fa53ba24c5ab37386
kailash pantha <076bei017.kailash@pcampus.edu.np>
2025-01-06 10:57:08 +0545

    refactor

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(p)mro4fexz%$als(8@q^ob5_nij!8gwbw@a@%ng84fk&p)o05'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['.vercel.app', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost','*']
 
 
 # Application definition
